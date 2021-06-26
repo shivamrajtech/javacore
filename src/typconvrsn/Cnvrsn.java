d137b31ccf4e72c196db3db230aab1947ec842c2
shivamrajtech <68703512+shivamrajtech@users.noreply.github.com>
2021-06-26 12:21:54 +0530

    Typeconvrson

---
+++ src/typconvrsn/Cnvrsn.java
@@ -0,0 +1,14 @@
+package typconvrsn;
+
+public class Cnvrsn {
+    public static void main(String[] args) {
+        // implicit converson
+        int num = 20;
+        double num1 = num;
+        System.out.println(num);
+
+        //expict or narrowing
+        double num2= 30;
+        int num3=(int) num2;// force full converting the type
+    }
+}
